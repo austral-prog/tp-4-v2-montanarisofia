0dcdd411423da2b4a2bfa188c918872fd9aa9143
Juan Longo <juan.longo@mulesoft.com>
2024-08-27 17:01:50 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -3,4 +3,4 @@ def leap_year():
     if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
         print(f"El año {year} es bisiesto")
     else:
-        print(f"El año {year} no es bisiesto")
+        print(f"El año {year} no es bisiesto") 
